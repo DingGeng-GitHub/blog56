ea4bdcaf22ab7a4bdafb16d2861ed3ffc7d5b49e
dg <707170913@qq.com>
2018-10-29 23:31:10 +0800

    将后台的login模型改为users模型

---
+++ app/Admin/Models/Users.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Admin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    //用户表的模型
+
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+
+}
